3d9d4130e43dd4ba466e8aaa5047196f895ea60a
darsh-oncloud <dkothari@oncloudconsulting.ca>
2026-04-16 14:41:58 -0400

    Update from NetSuite (Auto)

--- Ollie_Get_Celiigo_Errors.js
+++ Ollie_Get_Celiigo_Errors.js
@@ -2,9 +2,9 @@
  * @NApiVersion 2.1
  * @NScriptType MapReduceScript
  */
-define(['N/https', 'N/file', 'N/log'], function (https, file, log) {
+define(['N/https', 'N/file', 'N/log', 'N/search'], function (https, file, log, search) {
 
-    var CELIGO_TOKEN = 'c5925a41e3f04375961bd526d1125d54';
+    var CELIGO_TOKEN = 'PASTE_YOUR_TOKEN_HERE';
     var FLOW_ID = '6862d38aa2817b33330ea03f';
     var STEP_ID = '6862d383b5e58e8daab962dc';
     var FOLDER_ID = 3317;
@@ -13,13 +13,43 @@ define(['N/https', 'N/file', 'N/log'], function (https, file, log) {
         return (val || 'blank').toString().replace(/[^a-zA-Z0-9_\-]/g, '_');
     }
 
+    function getLast30MinUTC() {
+        var dt = new Date();
+        dt.setMinutes(dt.getMinutes() - 30);
+        return dt.toISOString();
+    }
+
+    function fileExists(fileName) {
+        var fileSearch = search.create({
+            type: 'file',
+            filters: [
+                ['name', 'is', fileName],
+                'and',
+                ['folder', 'anyof', FOLDER_ID]
+            ],
+            columns: ['internalid']
+        });
+
+        var results = fileSearch.run().getRange({
+            start: 0,
+            end: 1
+        });
+
+        return results && results.length > 0;
+    }
+
     function getInputData() {
         try {
+            var fromDate = getLast30MinUTC();
+            var toDate = new Date().toISOString();
+
             var url = 'https://api.integrator.io/v1/flows/' + FLOW_ID + '/' + STEP_ID + '/errors'
-                + '?occurredAt_gte=' + encodeURIComponent('2026-04-14T00:00:00.000Z')
-                + '&occurredAt_lte=' + encodeURIComponent(new Date().toISOString());
+                + '?occurredAt_gte=' + encodeURIComponent(fromDate)
+                + '&occurredAt_lte=' + encodeURIComponent(toDate);
 
             log.audit('Celigo URL', url);
+            log.audit('From Date', fromDate);
+            log.audit('To Date', toDate);
 
             var response = https.get({
                 url: url,
@@ -50,6 +80,11 @@ define(['N/https', 'N/file', 'N/log'], function (https, file, log) {
         try {
             var errorObj = JSON.parse(context.value);
             var fileName = safe(errorObj.traceKey) + '.json';
+
+            if (fileExists(fileName)) {
+                log.audit('File Already Exists', fileName);
+                return;
+            }
 
             var jsonFile = file.create({
                 name: fileName,
